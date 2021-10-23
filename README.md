c1b2b6ca573d9994822328a67ea23bcd96570fdb
Fernando Lopez <fer.ragnar19@gmail.com>
2021-10-22 21:29:33 -0300

    add routes

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 #### Api Rest Rust
 
+Si un número es primo o no.
+
 ```sh
 / (GET)
+/api/<number> (GET)
+
+Reemplazar number por un número
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
-use rocket::serde::json::{json, Value};
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
+use rocket::serde::json::{json, Value};
+
+// utils
+fn number_prime(n: u64) -> bool {
+    if n <= 1 {
+        return false;
+    }
+
+    for a in 2..n {
+        if n % a == 0 {
+            return false;
+        }
+    }
+
+    true
+}
+
+// Routes and controllers
 #[get("/")]
-fn index() -> Value {
+fn home() -> Value {
     json!({
-        "message": "Hola mundo",
-        "value": 2 + 2
+        "status": "ok",
+        "message": "Hola Mundo!"
     })
 }
 
+#[get("/api/<number>")]
+fn index(number: u64) -> Value {
+    json!({
+        "message": "Hola mundo",
+        "es primo": number_prime(number)
+    })
+}
+
+// Main Launch
 #[rocket::main]
 async fn main() {
-    rocket::build()
-        .mount("/", routes![index])
-        .launch()
-        .await;
+    rocket::build().mount("/", routes![index, home]).launch().await;
 }
